b967430052be66cc40ce7212252d29ca6141611f
Muhammadamin <muhammadamincoder@gmail.com>
2024-06-05 22:00:10 +0500

    initial commit

---
+++ lesson-1.py
@@ -0,0 +1,25 @@
+print("Day 1 - Python Print Function")
+print("The Function declared like this:")
+
+print("print ('what to print')")
+print("Hello world\nHello world\n")
+print("I" + " am")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign')
+print('e.g. print("Hello " + "World")')
+print("New lines can be created with a backslash and n.")
+
+print(len(input("What is your name?: ")))
+
+a = input("enter a value: ")
+b = input("enter b value: ")
+c = a
+a = b
+b = c
+print("a = " + a + "\nb = " + b)
+
+print("This program may find a band name generator")
+hometown = input("What is your hometown?\n")
+pet_name = input("What is you pet name?\n")
+print("Your brand name can be " + pet_name + " " + hometown)
